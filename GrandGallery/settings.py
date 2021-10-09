57ddcba73f29c7aff66f038b04f61fb72900bae6
kevaf <kevinkipyegon20@gmail.com>
2021-10-09 15:49:40 +0300

    innitial commit

--- GrandGallery/settings.py
+++ GrandGallery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$6yd8a2e()b&0xeramt7@gu4k(4jskcoz$tgs&!jgrp(g5k07l'
+SECRET_KEY = 'l0&u_4o93oe95x%q#g5dxv)afzg@&sa)l94j#_%ebme2#_1!@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
