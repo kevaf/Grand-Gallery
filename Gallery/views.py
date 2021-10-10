330ec0b34248ee7fd34aefa1591af140c6e582ad
kevaf <kevinkipyegon20@gmail.com>
2021-10-10 15:28:36 +0300

    added project and app structure

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+import datetime as dt
+from django.shortcuts import render, redirect
+from django.http  import HttpResponse
 
 # Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
