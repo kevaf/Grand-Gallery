203ead40769654f89b70b2bc5a129201e658e101
kevaf <kevinkipyegon20@gmail.com>
2021-10-11 10:05:02 +0300

    defined url paths

--- Gallery/urls.py
+++ Gallery/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^single_pic(\d+)',views.single_pic,name = 'single_pic'),
+    url(r'^search/',views.search_results,name = 'search_results'),
+    url(r'^category/(\d+)',views.viewPics_by_category, name = 'categorypic')
 ]
+
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
