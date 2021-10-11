91c0f1a6aa0a5eee46819b79949be04647f50356
kevaf <kevinkipyegon20@gmail.com>
2021-10-11 09:58:56 +0300

    added views

--- Gallery/views.py
+++ Gallery/views.py
@@ -1,7 +1,43 @@
 import datetime as dt
 from django.shortcuts import render, redirect
-from django.http  import HttpResponse
+from django.http  import HttpResponse, Http404
+from .models import Author, Images, Category
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def pics(request):
+    category = Category.get_categories()
+    pictures = Images.all_pics()
+
+
+    return render(request,'pics.html',{'pictures': pictures, 'category': category })
+
+
+def single_pic(request,id):
+    try:
+        pic = Images.objects.get(id = id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,"single_pic.html", {"pic":pic})
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get('image')
+        searched_pics = Images.search_by_name(search_term)
+        message = f'{search_term}'
+
+        return render(request,'search.html',{"message":message,"image":searched_pics})
+
+    else:
+        message = "You have not entered anything to search"
+        return render(request,'search.html',{"message":message})
+
+
+
+
+def viewPics_by_category(request,category):
+    photos =Images.view_pictures_by_category(category)
+    return render (request,'category.html',{"photos":photos})
