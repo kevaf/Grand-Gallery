e3d02653418296ce5a1ae267b8cd7b0b53be3c0c
kevaf <kevinkipyegon20@gmail.com>
2021-10-11 09:48:33 +0300

    added model class for authors

--- Gallery/models.py
+++ Gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=300, blank=True)
+
+
+    def __str__(self):
+        return self.first_name
