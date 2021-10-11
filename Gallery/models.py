1aa2b5d6bebd07da83bce86b6937baed05df31b9
kevaf <kevinkipyegon20@gmail.com>
2021-10-11 09:53:50 +0300

    added to models

--- Gallery/models.py
+++ Gallery/models.py
@@ -11,3 +11,68 @@ class Author(models.Model):
 
     def __str__(self):
         return self.first_name
+
+class Images(models.Model):
+    image = models.ImageField(upload_to = 'pictures/')
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=50)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE, default=None)
+    author = models.ForeignKey(Author,on_delete = models.CASCADE)
+
+    def save_pic(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+
+    
+    @classmethod
+    def all_pics(cls):
+        pics = cls.objects.all()
+        return pics
+
+    @classmethod
+    def get_one_pic(cls,id):
+        pictures = cls.objects.filter(id = id)
+        return pictures
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        image = cls.objects.filter(name__icontains=search_term)
+        return image
+
+    
+    @classmethod
+    def view_pictures_by_location(cls,location):
+        location_pics = cls.objects.filter(location= location)
+
+        return location_pics
+
+    @classmethod
+    def view_pictures_by_category(cls,category):
+        category = cls.objects.filter(category = category)
+        return category
+
+    
+
+
+
+    
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=80)
+
+
+
+    def save_category(self):
+        self.save()
+        
+    @classmethod
+    def get_categories(cls):
+        categories = cls.objects.all()
+
+        return categories
+
+    def __str__(self):
+        return self.category_name 
